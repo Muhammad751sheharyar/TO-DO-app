a17ec7faec55f04739b2316edb95b97adb49665a
Muhammad751sheharyar <sheharyar117794@gmail.com>
2025-09-04 23:46:03 +0500

    again

--- script.js
+++ script.js
@@ -2,15 +2,13 @@ function Add() {
     let detail = document.getElementById("detail").value;
     let li = document.createElement("li");
     let litext=document.createTextNode(li);
-    // console.log(litext)
-    // li.innerText = detail;
+    li.innerText = detail;
     let edit = document.createElement("button");
     edit.innerText = "Edit";
     let Delete = document.createElement("button");
     Delete.innerText = "Delete";
     let main = document.getElementById("main");
     main.append(li);
-    li.appendChild(litext);
     li.append(edit);
     li.append(Delete);
 
@@ -23,8 +21,7 @@ function Add() {
     edit.addEventListener('click',function(){
         let EditAgain=prompt("ente the update value");
         litext.innerText=EditAgain;
-    });
-
+    })
 
     Delete.addEventListener('click', function () {
         li.remove();
