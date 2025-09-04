7446bb4cbbb97e18eb3c5ed1c674823ba4b69ad7
Muhammad751sheharyar <sheharyar117794@gmail.com>
2025-09-04 23:33:58 +0500

    first

---
+++ script.js
@@ -0,0 +1,35 @@
+function Add() {
+    let detail = document.getElementById("detail").value;
+    let li = document.createElement("li");
+    let litext=document.createTextNode(li);
+    // console.log(litext)
+    // li.innerText = detail;
+    let edit = document.createElement("button");
+    edit.innerText = "Edit";
+    let Delete = document.createElement("button");
+    Delete.innerText = "Delete";
+    let main = document.getElementById("main");
+    main.append(li);
+    li.appendChild(litext);
+    li.append(edit);
+    li.append(Delete);
+
+
+
+
+
+
+    
+    edit.addEventListener('click',function(){
+        let EditAgain=prompt("ente the update value");
+        litext.innerText=EditAgain;
+    });
+
+
+    Delete.addEventListener('click', function () {
+        li.remove();
+    });
+
+}
+
+
